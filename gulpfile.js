8b74f3ecae83eb2e362545bb48217d9bd603fe1f
Vidal Rodrigo <vidalrodrigoti@gmail.com>
2020-10-28 21:56:53 -0300

    Layou universidad

---
+++ gulpfile.js
@@ -0,0 +1,22 @@
+const gulp = require ('gulp');
+const sass = require ('gulp-sass');
+const autoprefixer = require ('gulp-autoprefixer');
+
+function css(){
+    return gulp
+    .src('scss/app.scss')
+    .pipe(sass({
+        outputStyle: 'expanded', // nested, compact, compressed
+    }))
+    .pipe(gulp.dest('css'));
+}
+
+function watchFiles(){
+    gulp.watch('scss/*.scss', css);
+    gulp.watch('index.html');
+}
+
+//registrar funciones como tareas
+
+gulp.task('css', css);
+gulp.task('watch',gulp.parallel(watchFiles));
